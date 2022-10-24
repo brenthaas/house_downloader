0a5ad60f1fc634659c3c28633895fe10d49ec2d0
Brent Haas <brenthaas@gmail.com>
2022-10-24 10:34:31 -0700

    update prices that are sufficiently old

--- update_prices.js
+++ update_prices.js
@@ -1,7 +1,7 @@
 import { JSDOM } from "jsdom";
 import fetch from "node-fetch";
-import readlineSync from "readline-sync";
 import { Client } from "@notionhq/client";
+import moment from "moment";
 
 const fetchPage = (url) =>
   fetch(url)
@@ -14,10 +14,19 @@ const getHousesWithoutPrices = async () => {
   const query = {
     database_id: databaseId,
     filter: {
-      property: "Sold Price",
-      number: {
-        is_empty: true,
-      },
+      and: [
+        {
+          property: "Sold Price",
+          number: {
+            is_empty: true,
+          }
+        }, {
+          property: "Created At",
+          date: {
+            before: moment().subtract(2, "weeks").format("YYYY-MM-DD")
+          }
+        }
+      ]
     },
     sorts: [
       {
@@ -41,21 +50,54 @@ const parseSalePrice = (document) => {
   const lastSaleString = document.querySelector("div.secondary-info").textContent;
   let salePriceRegex =
     /This home last sold for (?<price>\$[,\d]+) on (?<saleDate>.*)$/;
-  let groups = lastSaleString.match(salePriceRegex).groups
-  return [groups.price, groups.saleDate]
+  if(lastSaleString.match(salePriceRegex) == null) {
+    return ['$0', 'Dec 31, 1999.']
+  }
+  let groups = lastSaleString.match(salePriceRegex).groups;
+  return [groups.price, groups.saleDate];
 }
 
 const getSalePrice = async (url) => {
-  console.log("Fetching ", url, " ......")
+  console.log("Fetching ", url, " ......");
   const pageContent = await fetchPage(url);
-  return parseSalePrice(pageContent)
+  return parseSalePrice(pageContent);
+}
+
+const updatePriceInNotion = async (pageId, salePrice, saleDate) => {
+  const response = await notion.pages.update({
+    page_id: pageId,
+    properties: {
+      "Sold Price": {
+        number: salePrice,
+      },
+       "Sold Date": {
+        date: {
+          start: saleDate
+        }
+      },
+    },
+  });
 }
 
 
+// ~~~~~~~~~~~~~ Main Script ~~~~~~~~~~~~~
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
-let houses = await getHousesWithoutPrices()
-let houseUrls = houses.map( (result) => (result.properties['URL'].url));
+let houses = await getHousesWithoutPrices();
 
-const [price, date] = await getSalePrice(houseUrls[0]);
+houses.forEach( async (house) => {
+  const url = house.properties['URL'].url;
+  const title = house.properties.Description.title[0].plain_text
+  const [price, date] = await getSalePrice(url);
+  const parsedDate = moment(date, "MMM D, YYYY.", true);
+  if(moment().diff(parsedDate, 'months') < 24) {
+    console.log(`${title} sold on ${parsedDate.format('MMMM DD YYYY')} for ${price}`);
+    updatePriceInNotion(
+      house.id,
+      parseInt(price.replaceAll(/[$, ]/gi, "")),
+      parsedDate.format('YYYY-MM-DD')
+    );
+  } else {
+    console.log(`${title} last sold on ${parsedDate.format('MMMM DD YYYY')}... not updating`)
+  }
+})
 
-console.log("Got Sale Price: ", price, " on date ", date);
